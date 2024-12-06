4950641e363c5465aa0264d1f31e1f3ef2742786
RehanALiBalti <adarvaish0@gmail.com>
2024-12-06 16:02:13 +0500

    add field in models

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -81,6 +81,138 @@ module.exports = (sequelize, Sequelize) => {
     role: {
       type: Sequelize.STRING,
     },
+    preferedCar1: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    preferedCar2: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    preferedCar3: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car0carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car0make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car0model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car0year: {
+      type: Sequelize.STRING, // Use INTEGER for year
+      allowNull: true,
+    },
+    car0cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car1carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car1make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car1model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car1year: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car1cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car2carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car2make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car2model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car2year: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car2cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car3carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car3make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car3model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car3year: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car3cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car4carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car4make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car4model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car4year: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car4cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car5carId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car5make: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car5model: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car5year: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    car5cardSpec: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
   });
 
   return User;
