7faf60a58508c5955b4fb75a7125a2f5e571ed4a
RehanALiBalti <adarvaish0@gmail.com>
2025-02-05 16:12:34 +0500

    updb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "terraform-20250204113159431200000001.cfkywe2k2d3m.eu-central-1.rds.amazonaws.com",
   USER: "buzzware",
   PASSWORD: "StrongPass123!",
-  DB: "buzzware_revvdout",
+  DB: "revvdout_db",
   dialect: "mysql",
   pool: {
     max: 5,
