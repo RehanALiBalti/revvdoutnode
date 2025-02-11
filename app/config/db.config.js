a42bb8aae31c1cf5241e19ab4064d16234136a37
RehanALiBalti <adarvaish0@gmail.com>
2025-02-11 18:28:14 +0500

    add db connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "terraform-20250204113159431200000001.cfkywe2k2d3m.eu-central-1.rds.amazonaws.com",
-  USER: "buzzware",
-  PASSWORD: "StrongPass123!",
-  DB: "revvdout_db",
+  HOST: "74.220.219.152",
+  USER: "buzzware_revvdout",
+  PASSWORD: "a=^M7]]mMLoR",
+  DB: "buzzware_revvdout",
   dialect: "mysql",
   pool: {
     max: 5,
